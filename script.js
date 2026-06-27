bada5070689aac2e427cc0c3c617b6a6479a597e
CaioFBim <caio.bim@escola.pr.gov.br>
2026-06-26 22:40:36 -0300

    Louco a

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    // 1. Pegar os elementos que vamos interagir do HTML
-    const btnProximo = document.querySelector('.btn-proximo');
+    // 1. Elementos (As Páginas)
     const telaHome = document.getElementById('home');
     const telaDefinicoes = document.getElementById('secao-definicoes');
+    const telaTopico3 = document.getElementById('secao-topico-3');
 
-    // 2. Escutar o momento exato em que o usuário clica no botão
-    btnProximo.addEventListener('click', () => {
-        
-        // Esconde a tela inicial
+    // 2. Elementos (Os Botões)
+    const btnIniciar = document.getElementById('btn-iniciar');
+    const btnProximoTopico = document.getElementById('btn-proximo-topico');
+    const btnVoltarInicio = document.getElementById('btn-voltar-inicio');
+
+    // 3. Ação: Da Página 1 para a Página 2
+    btnIniciar.addEventListener('click', () => {
         telaHome.classList.add('escondida');
-        
-        // Mostra a tela de definições
         telaDefinicoes.classList.remove('escondida');
-        
-        // Joga o scroll da tela automaticamente para o topo do novo slide
+        window.scrollTo(0, 0);
+    });
+
+    // 4. Ação: Da Página 2 para a Página 3
+    btnProximoTopico.addEventListener('click', () => {
+        telaDefinicoes.classList.add('escondida');
+        telaTopico3.classList.remove('escondida');
+        window.scrollTo(0, 0);
+    });
+
+    // 5. Ação: Da Página 3 de volta para o Início
+    btnVoltarInicio.addEventListener('click', () => {
+        telaTopico3.classList.add('escondida');
+        telaHome.classList.remove('escondida');
         window.scrollTo(0, 0);
     });
 });
