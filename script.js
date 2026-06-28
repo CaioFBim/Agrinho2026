614e0e321413f541974c994567e9f7816f1c1ff5
CaioFBim <caio.bim@escola.pr.gov.br>
2026-06-28 15:37:00 -0300

    Acho que foi agora!

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-// Selecionando os botões
+console.log("Sistema atualizado com sucesso!");
+
 const btnIniciar = document.getElementById('btn-iniciar');
 const btnIrPag3 = document.getElementById('btn-ir-pag3');
 const btnIrPag4 = document.getElementById('btn-ir-pag4');
 const btnVoltarInicio = document.getElementById('btn-voltar-inicio');
 
-// Selecionando as telas
 const telaHome = document.getElementById('home');
 const telaPag2 = document.getElementById('secao-definicoes');
 const telaPag3 = document.getElementById('secao-importancia');
 const telaPag4 = document.getElementById('secao-sustentabilidade');
 
-// Navegação Pág 1 -> Pág 2
 btnIniciar.addEventListener('click', () => {
     telaHome.classList.add('escondida');
     telaPag2.classList.remove('escondida');
-    window.scrollTo(0, 0); // Faz a página voltar pro topo
+    window.scrollTo(0, 0); 
 });
 
-// Navegação Pág 2 -> Pág 3
 btnIrPag3.addEventListener('click', () => {
     telaPag2.classList.add('escondida');
     telaPag3.classList.remove('escondida');
     window.scrollTo(0, 0);
 });
 
-// Navegação Pág 3 -> Pág 4
 btnIrPag4.addEventListener('click', () => {
     telaPag3.classList.add('escondida');
     telaPag4.classList.remove('escondida');
     window.scrollTo(0, 0);
 });
 
-// Navegação Pág 4 -> Volta para Home (Início)
 btnVoltarInicio.addEventListener('click', () => {
     telaPag4.classList.add('escondida');
     telaHome.classList.remove('escondida');
