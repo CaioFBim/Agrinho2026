f7cd5f9e86d8adc59d02a5932269bcb44813544b
CaioFBim <caio.bim@escola.pr.gov.br>
2026-06-28 15:19:40 -0300

    legal

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    // 1. Elementos (As Páginas)
-    const telaHome = document.getElementById('home');
-    const telaDefinicoes = document.getElementById('secao-definicoes');
-    const telaTopico3 = document.getElementById('secao-topico-3');
+// Selecionando os botões
+const btnIniciar = document.getElementById('btn-iniciar');
+const btnIrPag3 = document.getElementById('btn-ir-pag3');
+const btnIrPag4 = document.getElementById('btn-ir-pag4');
+const btnVoltarInicio = document.getElementById('btn-voltar-inicio');
 
-    // 2. Elementos (Os Botões)
-    const btnIniciar = document.getElementById('btn-iniciar');
-    const btnProximoTopico = document.getElementById('btn-proximo-topico');
-    const btnVoltarInicio = document.getElementById('btn-voltar-inicio');
+// Selecionando as telas
+const telaHome = document.getElementById('home');
+const telaPag2 = document.getElementById('secao-definicoes');
+const telaPag3 = document.getElementById('secao-importancia');
+const telaPag4 = document.getElementById('secao-sustentabilidade');
 
-    // 3. Ação: Da Página 1 para a Página 2
-    btnIniciar.addEventListener('click', () => {
-        telaHome.classList.add('escondida');
-        telaDefinicoes.classList.remove('escondida');
-        window.scrollTo(0, 0);
-    });
+// Navegação Pág 1 -> Pág 2
+btnIniciar.addEventListener('click', () => {
+    telaHome.classList.add('escondida');
+    telaPag2.classList.remove('escondida');
+    window.scrollTo(0, 0); // Faz a página voltar pro topo
+});
 
-    // 4. Ação: Da Página 2 para a Página 3
-    btnProximoTopico.addEventListener('click', () => {
-        telaDefinicoes.classList.add('escondida');
-        telaTopico3.classList.remove('escondida');
-        window.scrollTo(0, 0);
-    });
+// Navegação Pág 2 -> Pág 3
+btnIrPag3.addEventListener('click', () => {
+    telaPag2.classList.add('escondida');
+    telaPag3.classList.remove('escondida');
+    window.scrollTo(0, 0);
+});
 
-    // 5. Ação: Da Página 3 de volta para o Início
-    btnVoltarInicio.addEventListener('click', () => {
-        telaTopico3.classList.add('escondida');
-        telaHome.classList.remove('escondida');
-        window.scrollTo(0, 0);
-    });
+// Navegação Pág 3 -> Pág 4
+btnIrPag4.addEventListener('click', () => {
+    telaPag3.classList.add('escondida');
+    telaPag4.classList.remove('escondida');
+    window.scrollTo(0, 0);
+});
+
+// Navegação Pág 4 -> Volta para Home (Início)
+btnVoltarInicio.addEventListener('click', () => {
+    telaPag4.classList.add('escondida');
+    telaHome.classList.remove('escondida');
+    window.scrollTo(0, 0);
 });
